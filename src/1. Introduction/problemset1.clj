2952b53f1c13c6bf5fefb9455ed0c2e8bd0a9d0d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-09-05 17:02:44 -0500

    Factorial using recursion

---
+++ src/1. Introduction/problemset1.clj
@@ -0,0 +1,25 @@
+; Some code to solve some of the problems in
+; problem set #1.
+
+(require '[clojure.test :refer [deftest is run-tests]])
+
+(defn !
+  "Returns the factorial of n."
+  [n]
+  (if (zero? n)
+    1
+    (*' n (! (dec n)))))
+
+(deftest test-!
+  (is (= 1
+         (! 0)))
+  (is (= 120
+         (! 5)))
+  (is (= '(1 1 2 6 24 120 720 5040 40320 362880 3628800)
+         (map ! (range 11))))
+  (is (= 15511210043330985984000000N
+         (! 25)))
+  (is (= 815915283247897734345611269596115894272000000000N
+         (! 40))))
+
+(run-tests)
