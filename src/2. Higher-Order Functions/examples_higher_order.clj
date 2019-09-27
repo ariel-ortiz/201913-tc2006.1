77c5f1f44d279d5fe430cafe551ec805b7ea6812
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-09-26 19:07:52 -0500

    Examples of higher-order functions

---
+++ src/2. Higher-Order Functions/examples_higher_order.clj
@@ -0,0 +1,39 @@
+(defn my-map
+  "Returns a list resulting from applying fun to each
+  element in lst."
+  [fun lst]
+  (if (empty? lst)
+    ()
+    (cons (fun (first lst))
+          (my-map fun (rest lst)))))
+
+(defn make-mul-fun
+  "Makes a new multiply by x function."
+  [x]
+  (fn [y]
+    (* x y)))
+
+(defn fun
+  [a b c d e]
+  (* (+ a b) (/ c d) e))
+
+(defn fun-curry
+  [a]
+  (fn [b]
+    (fn [c]
+      (fn [d]
+        (fn [e]
+          (* (+ a b) (/ c d) e))))))
+
+
+(defn composite
+  "Returns a composite function of f and g."
+  [f g]
+  (fn [x]
+    (f (g x))))
+
+(defn f1 [x] (* x x))
+(defn f2 [x] (+ x 3))
+(def f3 (composite f1 f2))
+(def f4 (composite f2 f1))
+(def f5 (composite f4 f3))
