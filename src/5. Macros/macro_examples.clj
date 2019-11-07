ce489bfe78698d4afefc1d1c6cbd9323057790e0
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-11-07 17:45:04 -0600

    IF macro

--- src/5. Macros/macro_examples.clj
+++ src/5. Macros/macro_examples.clj
@@ -1,4 +1,5 @@
 (defmacro my-and
+  "Our own implementation of the and macro."
   ([] true)
   ([x] x)
   ([x & exprs]
@@ -6,3 +7,24 @@
        (if t#
          (my-and ~@exprs)
          t#))))
+
+(defn seek-delimited
+  "Returns a sequence with the elements contained in args that are
+  between start and end."
+  [args start end]
+  (->>
+    args
+    (drop-while #(not= % start))
+    rest
+    (take-while #(not= % end))))
+
+(defmacro IF
+  "Provide a conditional statement that is syntactically
+  a bit more similar to those found in languages like
+  Pascal or Fortran. It has the following form:
+
+  (IF condition :THEN exp1 exp2 ... :ELSE exp3 exp4 ...)"
+  [condition & args]
+  `(if ~condition
+     (do ~@(seek-delimited args :THEN :ELSE))
+     (do ~@(seek-delimited args :ELSE :THEN))))
